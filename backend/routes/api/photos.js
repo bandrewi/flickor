1d3b28f7df2a51f1ccab01ba58b47f736775b20d
andrew bui <bandrewi98@gmail.com>
2022-04-04 11:14:12 -0700

    get photo route

--- backend/routes/api/photos.js
+++ backend/routes/api/photos.js
@@ -5,4 +5,13 @@ const { Photo } = require('../../db/models');
 
 const router = express.Router();
 
+//Get photo
+router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const photo = await Photo.findByPk(id);
+
+    return res.json({ photo });
+}))
+
+
 module.exports = router
