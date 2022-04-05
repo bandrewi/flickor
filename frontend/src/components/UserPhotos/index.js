117be4168c88bd55ae9321f3fa02dd05db5e965e
andrew bui <bandrewi98@gmail.com>
2022-04-05 16:37:10 -0700

    finished user photos comp

--- frontend/src/components/UserPhotos/index.js
+++ frontend/src/components/UserPhotos/index.js
@@ -1,7 +1,32 @@
-import Photos from "./Photos";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
-export default function UserPhoto() {
+import { getUserPhotos } from "../../store/photo";
+
+export default function Photos() {
+    const dispatch = useDispatch();
+    const photos = useSelector(state => Object.values(state.photos))
+
+    useEffect(() => {
+        dispatch(getUserPhotos())
+    }, [dispatch])
+
     return (
-        <Photos />
+        <div>
+            {photos.length > 0 && (
+                <ul>
+                    {photos.map(photo =>
+                    (
+                        <li key={photo.id}>
+                            <Link to={`/photos/${photo.id}`}>
+                                <img src={photo.imageUrl} />
+                            </Link>
+                        </li>
+                    )
+                    )}
+                </ul>
+            )}
+        </div>
     )
 }
