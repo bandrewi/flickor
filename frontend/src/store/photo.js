82576d512e23b081d3a72dc81bf61e1eee661899
andrew bui <bandrewi98@gmail.com>
2022-04-05 22:17:30 -0700

    thunk function for edit photos

--- frontend/src/store/photo.js
+++ frontend/src/store/photo.js
@@ -2,11 +2,23 @@ import { csrfFetch } from "./csrf"
 
 const ADD = 'photo/ADD'
 const DELETE = 'photo/DELETE'
+const EDIT = 'photo/EDIT'
 const LOAD = 'photos/LOAD'
 const LOAD_ONE = '/photos/LOAD_ONE'
 
+
 const addPhoto = (photo) => ({
     type: ADD,
+    photo: photo.photo
+})
+
+const removePhoto = (id) => ({
+    type: DELETE,
+    id
+})
+
+const updatePhoto = photo => ({
+    type: EDIT,
     photo: photo.photo
 })
 
@@ -25,11 +37,32 @@ const loadPhotos = (photoList) => {
     }
 }
 
-const removePhoto = (id) => ({
-    type: DELETE,
-    id
-})
+export const uploadPhoto = (photo) => async (dispatch) => {
+    const { imageUrl, content } = photo
+    const res = await csrfFetch('/api/photos/new', {
+        method: 'POST',
+        body: JSON.stringify({ imageUrl, content })
+    })
+    const newPhoto = await res.json()
+    dispatch(addPhoto(newPhoto))
+}
 
+export const deletePhoto = (id) => async (dispatch) => {
+    const res = await csrfFetch(`/api/photos/${id}/delete`, {
+        method: 'DELETE'
+    })
+    dispatch(removePhoto(id))
+}
+
+export const editPhoto = (photo) => async (dispatch) => {
+    const { id, content } = photo
+    const res = await csrfFetch(`/api/photos/${id}/edit`, {
+        method: 'PUT',
+        body: JSON.stringify({ content })
+    })
+    const editedPhoto = await res.json()
+    dispatch(updatePhoto(editedPhoto))
+}
 
 export const getPhoto = (id) => async (dispatch) => {
     const res = await csrfFetch(`/api/photos/${id}`)
@@ -49,22 +82,6 @@ export const getUserPhotos = () => async (dispatch) => {
     dispatch(loadPhotos(photos))
 }
 
-export const uploadPhoto = (photo) => async (dispatch) => {
-    const { imageUrl, content } = photo
-    const res = await csrfFetch('/api/photos/new', {
-        method: 'POST',
-        body: JSON.stringify({ imageUrl, content })
-    })
-    const newPhoto = await res.json()
-    dispatch(addPhoto(newPhoto))
-}
-
-export const deletePhoto = (id) => async (dispatch) => {
-    const res = await csrfFetch(`/api/photos/${id}/delete`, {
-        method: 'DELETE'
-    })
-    dispatch(removePhoto(id))
-}
 
 const initializedState = {};
 
@@ -86,6 +103,10 @@ export default function photoReducer(state = initializedState, action) {
         case DELETE:
             newState = { ...state }
             delete newState[action.id]
+            return newState
+        case EDIT:
+            newState = { ...state }
+            newState[action.photo.id] = action.photo
             return newState
         default:
             return state
