292be9f8ef3b2fad6113357b68fbb9f1d6d16931
andrew bui <bandrewi98@gmail.com>
2022-04-05 17:25:28 -0700

    changed the err validations for edit route and removed imageurl as edit values

--- backend/routes/api/photos.js
+++ backend/routes/api/photos.js
@@ -18,6 +18,12 @@ validatePhoto = [
     handleValidationErrors
 ]
 
+validateEditPhoto = [
+    check('content')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a description for your image.'),
+    handleValidationErrors
+]
 //Get photos
 router.get('/', asyncHandler(async (req, res) => {
     const photos = await Photo.findAll({
@@ -65,12 +71,11 @@ router.post('/new', restoreUser, requireAuth, validatePhoto, asyncHandler(async 
 }))
 
 //Update photo
-router.put('/:id(\\d+)/edit', requireAuth, validatePhoto, asyncHandler(async (req, res) => {
+router.put('/:id(\\d+)/edit', requireAuth, validateEditPhoto, asyncHandler(async (req, res) => {
     const id = req.params.id;
-    const { imageUrl, content } = req.body
+    const { content } = req.body
     const photo = await Photo.findByPk(id);
 
-    photo.imageUrl = imageUrl;
     photo.content = content;
     await photo.save();
 
