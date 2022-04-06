55096356251155498d5020a8e4a854ad6141300e
andrew bui <bandrewi98@gmail.com>
2022-04-06 13:37:22 -0700

    userhome css

--- frontend/src/components/Home/UserHome.js
+++ frontend/src/components/Home/UserHome.js
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom'
 
 import Photo from '../Photo'
+import './UserHome.css'
 
 export default function UserHome({ photos }) {
 
     return (
         <div>
-            <ul>
+            <ul id='userhome-photo-container'>
                 {photos.map(photo => (
                     <li key={photo.id}>
                         <Link to={`/photos/${photo.id}`}>
