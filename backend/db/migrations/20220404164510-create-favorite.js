fc0af5be83dd28184f91aef73674e182005e7c37
andrew bui <bandrewi98@gmail.com>
2022-04-06 16:39:31 -0700

    fixed photId typo

--- backend/db/migrations/20220404164510-create-favorite.js
+++ backend/db/migrations/20220404164510-create-favorite.js
@@ -13,7 +13,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         references: { model: 'Users' }
       },
-      photId: {
+      photoId: {
         allowNull: false,
         type: Sequelize.INTEGER,
         references: { model: 'Photos' }
