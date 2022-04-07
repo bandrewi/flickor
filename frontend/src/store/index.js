d4edb6a51778e097dbca2f8794c385416878251d
andrew bui <bandrewi98@gmail.com>
2022-04-06 19:20:54 -0700

    added favorites reducer to combine reduceres

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import sessionReducer from './session'
 import photoReducer from "./photo";
+import favoriteReducer from "./favorite";
 
 const rootReducer = combineReducers({
   session: sessionReducer,
-  photos: photoReducer
+  photos: photoReducer,
+  favorites: favoriteReducer
 });
 
 let enhancer;
