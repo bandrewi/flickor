b6db8bb7ed5304101c462cff9905b81260e5b9bd
andrew bui <bandrewi98@gmail.com>
2022-04-08 01:15:20 -0700

    userhome css

--- frontend/src/components/Home/UserHome.js
+++ frontend/src/components/Home/UserHome.js
@@ -6,16 +6,16 @@ import './UserHome.css'
 export default function UserHome({ photos }) {
 
     return (
-        <div>
-            <ul id='userhome-photo-container'>
-                {photos.map(photo => (
-                    <li key={photo.id}>
-                        <Link to={`/photos/${photo.id}`}>
-                            <Photo photo={photo} />
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </div>
+        // <div>
+        <ul id='userhome-photo-container'>
+            {photos.map(photo => (
+                <li key={photo.id} className='photo-container'>
+                    <Link to={`/photos/${photo.id}`}>
+                        <Photo photo={photo} />
+                    </Link>
+                </li>
+            ))}
+        </ul>
+        // </div>
     )
 }
