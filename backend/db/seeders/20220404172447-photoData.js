aecce6f86ad4a88f5d8b46f12bced8e63ae262bd
andrew bui <bandrewi98@gmail.com>
2022-04-08 09:08:33 -0700

    seeder data changed

--- backend/db/seeders/20220404172447-photoData.js
+++ backend/db/seeders/20220404172447-photoData.js
@@ -11,48 +11,48 @@ module.exports = {
     return queryInterface.bulkInsert('Photos', [
       {
         userId: 1,
-        imageUrl: 'https://images.unsplash.com/photo-1649078805879-46792aacc351?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
-        content: 'Sakura Stroll'
+        imageUrl: 'https://images.unsplash.com/photo-1474511320723-9a56873867b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YW5pbWFsc3xlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60',
+        content: 'Fox'
       },
       {
         userId: 1,
-        imageUrl: 'https://images.unsplash.com/photo-1648933142932-47c00d284ecf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
-        content: 'Desert'
+        imageUrl: 'https://images.unsplash.com/photo-1425082661705-1834bfd09dca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGFuaW1hbHN8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        content: 'Hamster'
       },
       {
         userId: 1,
-        imageUrl: 'https://images.unsplash.com/photo-1648907833494-8c8e0eac7046?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3N3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
-        content: 'Ocean'
+        imageUrl: 'https://images.unsplash.com/photo-1437622368342-7a3d73a34c8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YW5pbWFsc3xlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60',
+        content: 'Turtle'
       },
       {
         userId: 2,
-        imageUrl: 'https://images.unsplash.com/photo-1649000942866-e89fd06ab3d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
-        content: 'Cookies'
+        imageUrl: 'https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG5hdHVyZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60',
+        content: 'Sunrise'
       },
       {
         userId: 2,
-        imageUrl: 'https://images.unsplash.com/photo-1649073344721-7b91565443c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
-        content: 'Burger'
+        imageUrl: 'https://images.unsplash.com/photo-1580137189272-c9379f8864fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDV8fG5hdHVyZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60',
+        content: 'Lake'
       },
       {
         userId: 2,
         imageUrl: 'https://images.unsplash.com/photo-1648912607168-88b8db633771?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
-        content: 'Oatmeal'
+        content: 'Yogurt'
       },
       {
         userId: 3,
-        imageUrl: 'https://images.unsplash.com/photo-1648974783311-a24c95296f5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzOHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
-        content: 'Skyscraper'
+        imageUrl: 'https://images.unsplash.com/photo-1509023464722-18d996393ca8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHdhbGxwYXBlcnN8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        content: 'Japan'
       },
       {
         userId: 3,
-        imageUrl: 'https://images.unsplash.com/photo-1649074799292-0f544ef7cdf6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
-        content: 'Pointy Bulding'
+        imageUrl: 'https://images.unsplash.com/photo-1505832018823-50331d70d237?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fHdhbGxwYXBlcnN8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        content: 'Train'
       },
       {
         userId: 3,
-        imageUrl: 'https://images.unsplash.com/photo-1648824964352-95fbc2e3bb0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5Nnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
-        content: 'Floofs'
+        imageUrl: 'https://images.unsplash.com/photo-1428908728789-d2de25dbd4e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHdhbGxwYXBlcnN8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        content: 'Sky'
       }
     ], {});
   },
