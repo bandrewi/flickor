d7bbd47488a021f9acbc572584b5e7461538e0e9
andrew bui <bandrewi98@gmail.com>
2022-06-09 17:37:18 -0700

    changed demo button to start for free and did css for it

--- frontend/src/components/Home/NonUserHome.js
+++ frontend/src/components/Home/NonUserHome.js
@@ -51,9 +51,9 @@ export default function NonUserHome({ photos }) {
                         <div id='nonuserhome-photo-container-inner' style={{ backgroundImage: `url(${images[photoNum]})` }}>
                             {/* <img id='nonuserhome-photo' src={photos[photoNum].imageUrl} /> */}
                             <h1 id='heading'>Find your inspiration.</h1>
-                            <h2 id="heading-2">Join the Flickor community, home to some photos and a couple users</h2>
+                            <h2 id="heading-2">Join the Flickor community, home to some photos and a couple users.</h2>
                             <div>
-                                <button id='demo-btn' onClick={handleClick}>Demo Login</button>
+                                <button id='demo-btn' onClick={handleClick}>Start for free</button>
                             </div>
                         </div>
                     </div>
