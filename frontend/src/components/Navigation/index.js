d672351da8f57da1e0ac7d591a37e702e65d8ed5
andrew bui <bandrewi98@gmail.com>
2022-06-10 14:09:51 -0700

    reworked navbar to render the whole navbar for user and nonuser

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -12,56 +12,62 @@ function Navigation({ isLoaded }) {
     let sessionLinks;
     if (sessionUser) {
         sessionLinks = (
-            <>
-                <ul id='user-left-nav'>
-                    <li id='user-flickor-li'>
-                        <img className='flickor-logo' src='https://i.imgur.com/SpPAHbL.png' alt='' />
-                        <NavLink exact to="/">flickor</NavLink>
-                    </li>
-                    <li id='your-photos'>
-                        <NavLink to='/photos'>Your Photos</NavLink>
-                    </li>
+            <div className='nav-bar user-nav'>
+                <ul id='nav-bar-ul'>
+                    <ul id='user-left-nav'>
+                        <li id='user-flickor-li'>
+                            <img className='flickor-logo' src='https://i.imgur.com/SpPAHbL.png' alt='' />
+                            <NavLink exact to="/">flickor</NavLink>
+                        </li>
+                        <li id='your-photos'>
+                            <NavLink to='/photos'>Your Photos</NavLink>
+                        </li>
+                    </ul>
+                    <ul id='user-right-nav'>
+                        <li id='nav-upload-btn'>
+                            <UploadButton />
+                        </li>
+                        <li id='profile-btn'>
+                            <ProfileButton user={sessionUser} />
+                        </li>
+                    </ul>
                 </ul>
-                <ul id='user-right-nav'>
-                    <li id='nav-upload-btn'>
-                        <UploadButton />
-                    </li>
-                    <li id='profile-btn'>
-                        <ProfileButton user={sessionUser} />
-                    </li>
-                </ul>
-            </>
+            </div>
         );
     } else {
         sessionLinks = (
-            <>
-                <li id='nonuser-flickor-li'>
-                    <img className='flickor-logo' src='https://i.imgur.com/SpPAHbL.png' alt='' />
-                    <NavLink exact to="/">flickor</NavLink>
-                </li>
-                {location.pathname === '/' && (
-                    < ul id='nonuser-btns'>
-                        <li id='nav-login-li'>
-                            <NavLink to="/login">Log In</NavLink>
-                        </li>
-                        <li id='nav-signup-li'>
-                            <NavLink to="/signup">
-                                <button id='nav-signup-btn'>Sign Up</button>
-                            </NavLink>
-                        </li>
-                    </ul>
-                )
-                }
-            </>
+            <div className='nav-bar'>
+                <ul id='nav-bar-ul'>
+                    <li id='nonuser-flickor-li'>
+                        <img className='flickor-logo' src='https://i.imgur.com/SpPAHbL.png' alt='' />
+                        <NavLink exact to="/">flickor</NavLink>
+                    </li>
+                    {location.pathname === '/' && (
+                        < ul id='nonuser-btns'>
+                            <li id='nav-login-li'>
+                                <NavLink to="/login">Log In</NavLink>
+                            </li>
+                            <li id='nav-signup-li'>
+                                <NavLink to="/signup">
+                                    <button id='nav-signup-btn'>Sign Up</button>
+                                </NavLink>
+                            </li>
+                        </ul>
+                    )
+                    }
+                </ul>
+            </div>
         );
     }
 
     return (
-        <div className='nav-bar'>
-            <ul id='nav-bar-ul'>
-                {isLoaded && sessionLinks}
-            </ul>
-        </div>
+        // <div className='nav-bar'>
+        //     <ul id='nav-bar-ul'>
+        <>
+            {isLoaded && sessionLinks}
+        </>
+        //     </ul>
+        // </div>
     );
 }
 
