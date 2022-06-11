cd74030b4d681d3c5530fde0b0a3a439281ce7cb
andrew bui <bandrewi98@gmail.com>
2022-06-11 03:45:39 -0700

    restoreUser thunk returns data.user

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -34,7 +34,7 @@ export const restoreUser = () => async dispatch => {
     const response = await csrfFetch('/api/session');
     const data = await response.json();
     dispatch(setUser(data.user));
-    return data;
+    return data.user;
 };
 
 export const signup = (user) => async (dispatch) => {
