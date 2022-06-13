955689adb5487f3a6b77652800e17e1189390ce8
andrew bui <bandrewi98@gmail.com>
2022-06-13 14:02:18 -0700

    FavoritesPage functional

---
+++ frontend/src/components/FavoritesPage/index.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { Link } from "react-router-dom"
+import { getFavorites } from "../../store/favorite"
+
+export default function FavoritesPage() {
+    const dispatch = useDispatch()
+    const favorites = useSelector(state => Object.values(state.favorites))
+
+    useEffect(() => {
+        dispatch(getFavorites())
+    }, [dispatch])
+
+    return (
+        <div id="userphotos-container-outer">
+            <ul id='userphotos-container'>
+                {favorites.map(favorite => (
+                    <li key={favorite.id}>
+                        <Link to={`/photos/${favorite.photoId}`}>
+                            <img className='photo user-photo' src={favorite.imageUrl} alt="" />
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
