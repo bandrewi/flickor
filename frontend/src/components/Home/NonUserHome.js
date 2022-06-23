5b3c253c015aee3679909922f25e79d9edd9c34f
andrew bui <bandrewi98@gmail.com>
2022-06-22 17:05:30 -0700

    images are taken from images directory

--- frontend/src/components/Home/NonUserHome.js
+++ frontend/src/components/Home/NonUserHome.js
@@ -6,14 +6,14 @@ import './NonUserHome.css'
 export default function NonUserHome() {
     const [photoNum, setPhotoNum] = useState(0);
     const images = [
-        'https://combo.staticflickr.com/ap/build/images/sohp/2020-top-25/If_Only_We_Could_Turn_Back_Time_Anna_Kwa.jpg',
-        'https://combo.staticflickr.com/ap/build/images/sohp/2020-top-25/Frosch_Bokeh_2_Axel_F.jpg',
-        'https://combo.staticflickr.com/ap/build/images/sohp/2020-top-25/Dawn_of_Another_Day_Sky_Matthews.jpg',
-        'https://combo.staticflickr.com/ap/build/images/sohp/2020-top-25/Fantasy_Island_Daniel_Cheong.jpg',
-        'https://combo.staticflickr.com/ap/build/images/sohp/2020-top-25/Desert_Beauty_Christoph_Fischer.jpg',
-        'https://combo.staticflickr.com/ap/build/images/sohp/2020-top-25/sunset_1663_Junji_Aoyama.jpg',
-        'https://combo.staticflickr.com/ap/build/images/sohp/2020-top-25/Cool_Power_commended-LPOTY_UK_Steve_Cole.jpg',
-        'https://combo.staticflickr.com/ap/build/images/sohp/2020-top-25/Mists_of_renfrew_Adam_Gibbs.jpg',
+        '/images/If_Only_We_Could_Turn_Back_Time_Anna_Kwa.jpg',
+        '/images/Frosch_Bokeh_2_Axel_F.jpg',
+        '/images/Dawn_of_Another_Day_Sky_Matthews.jpg',
+        '/images/Fantasy_Island_Daniel_Cheong.jpg',
+        '/images/Desert_Beauty_Christoph_Fischer.jpg',
+        '/images/sunset_1663_Junji_Aoyama.jpg',
+        '/images/Cool_Power_commended-LPOTY_UK_Steve_Cole.jpg',
+        '/images/Mists_of_renfrew_Adam_Gibbs.jpg'
     ]
 
     useEffect(() => {
